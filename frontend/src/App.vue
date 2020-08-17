cf405e2927c7e4ec2e3019638e012aaa4caa94a5
florian <florian.bopp90@gmail.com>
2020-08-17 13:27:52 +0200

    fixed frontend git error

---
+++ frontend/src/App.vue
@@ -0,0 +1,222 @@
+<template>
+  <v-app id="inspire">
+    <v-app id="inspire">
+      <v-navigation-drawer app clipped fixed permanent width="330px">
+        <v-list-item>
+          <v-list-item-content class="text-subtitle-2">Select Your Courses below</v-list-item-content>
+        </v-list-item>
+        <v-list dense>
+          <v-list-group v-for="(degree, h) in getCourses" :key="h" no-action>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>{{ degree.name }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-group v-for="(semester, i) in degree.semesters" :key="i" no-action sub-group>
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>{{ semester.name }}</v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item-group multiple>
+                <v-list-item
+                  v-for="(course, j) in semester.courses"
+                  :key="j"
+                  @input="toggleSelected($event, course)"
+                >
+                  <template v-slot:default="{ active }">
+                    <v-list-item-action>
+                      <v-checkbox :input-value="active" color="primary"></v-checkbox>
+                    </v-list-item-action>
+                    <v-list-item-title v-text="course.replace('__', '/').replace('_', ' ')"></v-list-item-title>
+                  </template>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list-group>
+          </v-list-group>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar app clipped-left flat>
+        <v-toolbar-title>HAW Calendar Vuer</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-switch
+          flat
+          label="Dark Theme"
+          color="red"
+          hide-details
+          v-model="themeSwitchToDark"
+          @change="toggleTheme"
+        ></v-switch>
+      </v-app-bar>
+
+      <v-main>
+        <v-container class="fill-height" fluid>
+          <v-row class="fill-height">
+            <v-col>
+              <v-sheet height="64">
+                <v-toolbar flat>
+                  <v-btn outlined class="mr-4" @click="setToday">Today</v-btn>
+                  <v-btn fab text small @click="prev">
+                    <v-icon small>mdi-chevron-left</v-icon>
+                  </v-btn>
+                  <v-btn class="ml-1" fab text small @click="next">
+                    <v-icon small>mdi-chevron-right</v-icon>
+                  </v-btn>
+
+                  <v-toolbar-title class="ml-3" v-if="$refs.calendar">{{ $refs.calendar.title }}</v-toolbar-title>
+                </v-toolbar>
+              </v-sheet>
+              <v-sheet style="height: 95%">
+                <v-calendar
+                  show-week
+                  ref="calendar"
+                  v-model="focus"
+                  color="primary"
+                  :events="getEvents"
+                  type="month"
+                  event-more
+                  :weekdays="weekdays"
+                  @click:event="showEvent"
+                ></v-calendar>
+                <v-menu
+                  v-model="selectedOpen"
+                  :close-on-content-click="false"
+                  :activator="selectedElement"
+                  offset-x
+                >
+                  <v-card color="grey lighten-5" min-width="350px" flat>
+                    <v-toolbar color="blue-grey darken-1" dark>
+                    
+                      <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                      <v-spacer></v-spacer>
+                     
+                     
+                    </v-toolbar>
+                    <v-card-text>
+                      <span v-html="selectedEvent.room"></span>
+                      <br>
+                      <span v-html="selectedEvent.lecturer"></span>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-btn text color="secondary" @click="selectedOpen = false">Cancel</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-menu>
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+
+      <v-footer app>
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+      </v-footer>
+    </v-app>
+  </v-app>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "App",
+  data: () => ({
+    eventInputCounter: 0,
+    focus: "",
+    type: "month",
+    typeToLabel: {
+      month: "Month",
+      week: "Week",
+      day: "Day",
+      "4day": "4 Days"
+    },
+    weekdays: [1, 2, 3, 4, 5, 6, 0],
+    selectedEvent: {},
+    selectedElement: null,
+    selectedOpen: false,
+    events: [],
+
+    themeSwitchToDark: false,
+    selectedCourses: [],
+    degrees: {
+      et: {
+        name: "Elektrotechnik",
+        courses: ["et1", "et2", "el1", "el2", "el3", "oop"]
+      },
+      re: {
+        name: "Regernerative Systeme",
+        courses: ["1", "2", "3", "4", "5", "6"]
+      }
+    }
+  }),
+  computed: {
+    ...mapGetters(["getCourses"]),
+    ...mapGetters(["getEvents"])
+  },
+  methods: {
+    ...mapActions(["fetchNames"]),
+    ...mapActions(["fetchCouseJsonData"]),
+    ...mapActions(["deleteEvents"]),
+
+    toggleTheme() {
+      this.$vuetify.theme.dark = this.themeSwitchToDark;
+    },
+    toggleSelected(payload, course) {
+      if (payload === true) {
+        setTimeout(() => {
+          this.eventInputCounter = 0;
+        }, 50);
+        if (this.eventInputCounter === 0) {
+          console.log(course);
+          this.fetchCouseJsonData(course);
+          this.eventInputCounter++;
+        }
+      } else {
+        console.log(course);
+        this.deleteEvents(course);
+      }
+    },
+
+    viewDay({ date }) {
+      this.focus = date;
+      this.type = "day";
+    },
+   
+    setToday() {
+      this.focus = "";
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        setTimeout(() => (this.selectedOpen = true), 10);
+      };
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        setTimeout(open, 10);
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
+    }
+  },
+  created() {
+    this.$vuetify.theme.dark = false;
+    this.fetchNames();
+  },
+  mounted() {
+    this.$refs.calendar.checkChange();
+  }
+};
+</script>
---
+++ frontend/src/store/index.js
@@ -0,0 +1,72 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import moment from "moment";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    courses: {},
+    events: [],
+    API_URL: process.env.API_URL || "http://localhost:80/api",
+  },
+  getters: {
+    getCourses: (state) => state.courses,
+    getEvents: (state) => state.events,
+  },
+  actions: {
+    async fetchNames({ state, commit }) {
+      try {
+        var response = await axios({
+          crossdomain: true,
+          method: "GET",
+          url: state.API_URL + "/courses/allnames",
+        });
+        console.log(response.data);
+        commit("setCourses", response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async fetchCouseJsonData({ state, commit }, course) {
+      try {
+        const response = await axios({
+          method: "GET",
+          url: `${state.API_URL}/courses/events/${course}`,
+        });
+        const eventdata = response.data.events.map(
+          ({ dstart, dend, ...rest }) => {
+            return {
+              start: moment(dstart).format("YYYY-MM-DD HH:mm"),
+              end: moment(dend).format("YYYY-MM-DD HH:mm"),
+              ...rest,
+            };
+          }
+        );
+        console.log(response);
+        commit("addEvents", eventdata);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    deleteEvents({ state, commit }, courseName) {
+      commit(
+        "deleteEvents",
+        state.events.filter((event) => event.name !== courseName)
+      );
+    },
+  },
+  mutations: {
+    setCourses: (state, data) => {
+      state.courses = { ...data.data };
+    },
+    addEvents: (state, data) => {
+      state.events.push(...data);
+    },
+    deleteEvents: (state, newEvents) => {
+      state.events = [...newEvents];
+    },
+  },
+  modules: {},
+});
