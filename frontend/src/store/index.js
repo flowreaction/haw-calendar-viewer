eb615841b1cf7341fb613bc32a8e04c546d1ecdf
florian <florian.bopp90@gmail.com>
2020-08-18 10:22:41 +0200

    fixed missing subroute

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -9,7 +9,7 @@ export default new Vuex.Store({
   state: {
     courses: {},
     events: [],
-    API_URL: window.location.host + "/api",
+    API_URL: `${window.location.protocol}//${window.location.host}/api`,
   },
   getters: {
     getCourses: (state) => state.courses,
